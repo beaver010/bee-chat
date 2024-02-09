a7caad651f364261d76ffa5d8b33b7fe706c90a8
beaver010 <151190068+beaver010@users.noreply.github.com>
2024-02-09 19:34:37 +0100

    Setup project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,20 @@
 plugins {
     kotlin("jvm") version "1.9.22"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 group = "com.github.beaver010"
-version = "1.0-SNAPSHOT"
+version = "1.0"
 
 repositories {
     mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
 }
 
 dependencies {
-    testImplementation("org.jetbrains.kotlin:kotlin-test")
+    compileOnly("io.papermc.paper:paper-api:1.20.4-R0.1-SNAPSHOT")
 }
 
-tasks.test {
-    useJUnitPlatform()
-}
 kotlin {
     jvmToolchain(17)
 }
---
+++ src/main/kotlin/com/github/beaver010/beechat/BeeChat.kt
@@ -0,0 +1,6 @@
+package com.github.beaver010.beechat
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class BeeChat: JavaPlugin() {
+}
