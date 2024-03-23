cdbc367cf1f71b1fffab76904592fe448a084530
beaver010 <151190068+beaver010@users.noreply.github.com>
2024-03-23 12:25:12 +0100

    Add CommandAPI dependency

--- src/main/kotlin/com/github/beaver010/beechat/BeeChat.kt
+++ src/main/kotlin/com/github/beaver010/beechat/BeeChat.kt
@@ -2,19 +2,23 @@ package com.github.beaver010.beechat
 
 import com.github.beaver010.beechat.listener.ChatListener
 import com.github.beaver010.beechat.listener.JoinListener
+import dev.jorel.commandapi.CommandAPI
+import dev.jorel.commandapi.CommandAPIBukkitConfig
 import org.bukkit.event.Listener
 import org.bukkit.plugin.java.JavaPlugin
 
 class BeeChat : JavaPlugin() {
     override fun onLoad() {
+        CommandAPI.onLoad(CommandAPIBukkitConfig(this).silentLogs(true))
+
         instance = this
     }
 
     override fun onEnable() {
+        CommandAPI.onEnable()
+
         saveDefaultConfig()
-
         Permissions.register()
-
         registerEvents(ChatListener)
 
         if (PluginConfig.tabListFormattingEnabled) {
@@ -29,6 +33,10 @@ class BeeChat : JavaPlugin() {
                 )
             }
         }
+    }
+
+    override fun onDisable() {
+        CommandAPI.onDisable()
     }
 
     private fun registerEvents(listener: Listener) {
