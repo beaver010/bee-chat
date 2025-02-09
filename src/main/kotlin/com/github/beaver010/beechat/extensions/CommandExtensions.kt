f0c33874baa1915eb0951a616e0f2933e11accfc
beaver010 <151190068+beaver010@users.noreply.github.com>
2025-02-09 12:59:55 +0100

    Fix command registration

--- src/main/kotlin/com/github/beaver010/beechat/extensions/CommandExtensions.kt
+++ src/main/kotlin/com/github/beaver010/beechat/extensions/CommandExtensions.kt
@@ -3,6 +3,6 @@ package com.github.beaver010.beechat.extensions
 import org.bukkit.Bukkit
 import org.bukkit.command.Command
 
-fun Command.register() {
-    this.register(Bukkit.getCommandMap())
+fun Command.register(fallbackPrefix: String = "beechat") {
+    Bukkit.getCommandMap().register(fallbackPrefix, this)
 }
